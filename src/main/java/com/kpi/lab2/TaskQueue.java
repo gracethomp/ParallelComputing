2436dbd45eb663b10fe8ccf7e924421614d3254d
Olena Babii <gracetomlinson26@gmail.com>
2023-04-09 21:15:19 +0300

    lab 2 is done

--- src/main/java/com/kpi/lab2/TaskQueue.java
+++ src/main/java/com/kpi/lab2/TaskQueue.java
@@ -1,4 +1,4 @@
-package com.kpi.lab2_1;
+package com.kpi.lab2;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
--- src/main/java/com/kpi/lab2/ThreadPool.java
+++ src/main/java/com/kpi/lab2/ThreadPool.java
@@ -1,4 +1,4 @@
-package com.kpi.lab2_1;
+package com.kpi.lab2;
 
 import java.util.ArrayList;
 import java.util.List;
